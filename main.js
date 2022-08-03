e5e775e6c0aff8a51e7a5957a2871201a0c81bfc
victor claros <victor.claros28.vc@gmail.com>
2022-08-03 13:15:34 -0500

    integrados el menu y el aside

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart")
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
-menuEmail.addEventListener("click", showMenu);
+menuEmail.addEventListener("click", showMenuDesktop);
+menuHamIcon.addEventListener("click", showMenuMobile);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
-function showMenu(){
+//mostrando menu de escritorio
+function showMenuDesktop(){
+    aside.classList.add("inactive");
     desktopMenu.classList.toggle("inactive");
+}
+//mostrando menu en movil
+function showMenuMobile(){
+    aside.classList.add("inactive");
+    mobileMenu.classList.toggle("inactive");
+}
+//mostrando el carrito de compras en movil y escritorio
+function toggleCarritoAside(){
+    aside.classList.toggle("inactive");
+    mobileMenu.classList.add("inactive");
+    desktopMenu.classList.add("inactive");
 }
